a6e07cb6cb55e2923b75be847176ebedabc6a344
hoo <dlwjdgn8720@gmail.com>
2026-05-06 18:27:30 +0900

    2026-05-03 commit

--- front/src/components/CompLogin.jsx
+++ front/src/components/CompLogin.jsx
@@ -1,32 +1,41 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { postFetchData } from '../util/fetchDatas';
 
 export default function CompLogin() {
     const [form, setForm] = useState({ id: '', password: '' });
+    const [cf, setCf] = useState(true);
+    const idRef = useRef(null);
+    const pwdRef = useRef(null);
+
     const style = {
         input: { padding: '10px', fontSize: '16px' },
-        button: { padding: '10px', backgroundColor: '#007bff', 
-                    color: 'white', border: 'none', cursor: 'pointer' },
-        form:{ display: "flex", flexDirection: "column", gap: "10px" }
+        button: {
+            padding: '10px', backgroundColor: '#007bff',
+            color: 'white', border: 'none', cursor: 'pointer'
+        },
+        form: { display: "flex", flexDirection: "column", gap: "10px" },
+        span: { color: "red", fontSize: '13px' }
     }
 
-    const handleChange = async (e) => {
-        e.preventDefault();
-        const{ name, value } = e.target;
-        setForm({...form, [name] : value})
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setForm({ ...form, [name]: value })
+        if(value !== '') setCf(false);    
+    }
+
+    const handleCfChange = (e) => {
+        const { value } = e.target;
+        
     }
 
     const handleLogin = async (e) => {
         e.preventDefault();
-        if(form.id === ''){
-            
-        }else if(form.password === ''){
-                
-        }else {
-            console.log(form);      
+        if (idRef.current.value.trim() !== '' && pwdRef.current.value.trim() !== '') {
             const response = await postFetchData(`/api/post`, form);
             response.result ? alert('로그인 성공') : alert('로그인 실패');
+        } else {
+            alert('아이디와 비밀번호는 필수값입니다!');
         }
     }
 
@@ -40,24 +49,26 @@ export default function CompLogin() {
                     placeholder="아이디를 입력하세요"
                     value={form.id}
                     onChange={handleChange}
-                    required
+                    ref={idRef}
+                    //required
                     style={style.input}
                 />
+                {cf && <span style={style.span}>아이디는 필수값입니다.</span>}
                 <input
                     type="password"
                     name="password"
                     placeholder="비밀번호를 입력하세요"
                     value={form.password}
                     onChange={handleChange}
-                    required
+                    ref={pwdRef}
+                    //required
                     style={style.input}
                 />
+                {cf && <span style={style.span}>비밀번호는 필수값입니다.</span>}
                 <button type="submit" style={style.button}>로그인</button>
             </form>
         </div>
 
     );
-
-    
 }
 
