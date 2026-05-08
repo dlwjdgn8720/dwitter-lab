cc4bd26c47880fe4b2f6bf779f6c4cc62faeec49
hoo <dlwjdgn8720@gmail.com>
2026-05-08 14:07:49 +0900

    2026-05-08

--- portfolio-front/src/App.jsx
+++ portfolio-front/src/App.jsx
@@ -5,21 +5,21 @@ import Footer from './components/Footer.jsx';
 import { Outlet } from 'react-router-dom';
 
 export default function App() {
-  const [data, setData] = useState({});  
+  // const [data, setData] = useState({});  
 
-  useEffect(()=>{
-    const loadData = async() => {
-      const response = await fetch("http://localhost:5173/data/portfolio.json");
-      const json = await response.json();
-      setData(json);
-    }
-    loadData();
-  }, []);
+  // useEffect(()=>{
+  //   const loadData = async() => {
+  //     const response = await fetch("http://localhost:5173/data/portfolio.json");
+  //     const json = await response.json();
+  //     setData(json);
+  //   }
+  //   loadData();
+  // }, []);
   
   return (
     <>
       <Header />
-      <Outlet context={{ data: data?.content }}/> 
+      <Outlet/> 
       <Footer/>
     </>
   )
--- portfolio-front/src/components/content/Projects.jsx
+++ portfolio-front/src/components/content/Projects.jsx
@@ -1,11 +1,19 @@
 import React, { useState } from 'react';
 import AvatarImage from '../commons/AvatarImage.jsx';
 import Modal from './Modal.jsx';
+import { getFetchData } from '../../util/fetch.js';
 
 export default function Projects({ projects }) {
     const [selectedProject, setSelectedProject] = useState(null);
     const handleClose = () => {
         setSelectedProject(null);
+    }
+
+    const handleProjectDetail = async(pid) => {
+        //http://localhost:9000/content/work/project/:pid
+        const jsonData = await getFetchData(`/content/work/project/${pid}`);
+        console.log(jsonData.result);
+        setSelectedProject(jsonData.result); //project {pid: "P001"...}
     }
 
     return (
@@ -14,7 +22,7 @@ export default function Projects({ projects }) {
                 {projects?.map((project, idx) => 
                     <li className="project" 
                         key={idx}
-                        onClick={()=> setSelectedProject(project)}>
+                        onClick={()=> handleProjectDetail(project.pid)}>
                         <AvatarImage    img={project.img}
                                         alt={project.alt}
                                         style="project-img" />
